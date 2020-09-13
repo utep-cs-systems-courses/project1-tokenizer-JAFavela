25368f8240cc637905beacbe77ea6a5805a8cb2a
JAFavela <jafavela@miners.utep.edu>
2020-09-13 13:27:13 -0600

    Added method placeholders

--- src/history.c
+++ src/history.c
@@ -2,3 +2,28 @@
 #include "history.h"
 
 //This file will include all the methods defined in history.h
+
+/* Add a history item to the end of the list.
+   List* list - the linked list
+   char* str - the string to store
+*/
+void add_history(List *list, char *str){
+  
+}
+
+/* Retrieve the string stored in the node where Item->id == id.
+   List* list - the linked list
+   int id - the id of the Item to find */
+char *get_history(List *list, int id){
+  return 0;
+}
+
+/*Print the entire contents of the list. */
+void print_history(List *list){
+  
+}
+
+/*Free the history list and the strings it references. */
+void free_history(List *list){
+  
+}
