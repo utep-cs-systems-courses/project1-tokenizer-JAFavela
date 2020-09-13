864f1d12896f6be71d7ad3a6f8613ecc39d90e7d
JAFavela <jafavela@miners.utep.edu>
2020-09-13 13:29:11 -0600

    Completed space_character and non_space_character methods

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -2,3 +2,63 @@
 #include "tokenizer.h"
 
 //This file will have all the methods defined in tokenizer.h
+
+/* Return true (non-zero) if c is a whitespace characer
+   ('\t' or' ').  
+   Zero terminators are not printable (therefore false) */
+int space_char(char c){
+  return c<'!';
+}
+
+/* Return true (non-zero) if c is a non-whitespace 
+   character (not tab or space).  
+   Zero terminators are not printable (therefore false) */ 
+int non_space_char(char c){
+  return c>='!';
+}
+
+/* Returns a pointer to the first character of the next 
+   space-separated word in zero-terminated str.  Return a zero pointer if 
+   str does not contain any words. */
+char word_start(char *str){
+  return 0;
+} 
+
+/* Returns a pointer terminator char following *word */
+char *word_terminator(char *word){
+  return 0;
+}
+
+/* Counts the number of words in the string argument. */
+int count_words(char *str){
+  return 0;
+}
+
+/* Returns a fresly allocated new zero-terminated string 
+   containing <len> chars from <inStr> */
+char *copy_str(char *inStr, short len){
+  return 0;
+}
+
+/* Returns a freshly allocated zero-terminated vector of freshly allocated 
+   space-separated tokens from zero-terminated str.
+
+   For example, tokenize("hello world string") would result in:
+     tokens[0] = "hello"
+     tokens[1] = "world"
+     tokens[2] = "string" 
+     tokens[3] = 0
+*/
+char **tokenize(char* str){
+  return 0;
+}
+
+/* Prints all tokens. */
+void print_tokens(char **tokens){
+  
+}
+
+/* Frees all tokens and the vector containing themx. */
+void free_tokens(char **tokens){
+  
+}
--- src/tokenizer.h
+++ src/tokenizer.h
@@ -8,7 +8,7 @@
 int space_char(char c);
 
 /* Return true (non-zero) if c is a non-whitespace 
-   character (not tab or space).  
+    character (not tab or space).  
    Zero terminators are not printable (therefore false) */ 
 int non_space_char(char c);
 
