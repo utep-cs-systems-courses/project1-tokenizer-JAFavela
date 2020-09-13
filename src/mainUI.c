8c0f3a96d9e53c9cd0d0dd5bdc33258302bc18fb
JAFavela <jafavela@miners.utep.edu>
2020-09-13 13:32:39 -0600

    modified UI file to test methods with input given

--- src/mainUI.c
+++ src/mainUI.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
+#include "tokenizer.h"
+#include "history.h"
 
 //This will be the main ui file
 
 int main()
 {
-  char str[50];
-  printf("> ");
+  /*char str[50];
+  printf("\n> ");
   scanf("%[^\n]",str);
   printf("%s\n",str);
+  */
+
+  while (1) { // Infinite while loop
+
+    fputs("\n> ", stdout);
+    fflush(stdout);/* stdout only flushes automatically on \n */
+    int c;
+    while ((c = getchar()) == '\n'); /* ignore newlines */
+    if (c == EOF)     /* terminate on end-of-file */
+      goto done;
+    
+    if(c=='~'){
+      puts("Bye!");
+      goto done; /* terminate */
+    }
+    else{
+      printf("sc : %d",space_char(c));
+      printf("\nnsc: %d\n",non_space_char(c));
+    }
+}
+ done: // To exit from program
   return 0;
 }
