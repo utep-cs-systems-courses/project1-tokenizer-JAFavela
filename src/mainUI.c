477c0909981e40ec4a8742f7d26c7f1bf3f054c4
JAFavela <jafavela@miners.utep.edu>
2020-09-14 00:30:53 -0600

    added code to run tests on remaining methods

--- src/mainUI.c
+++ src/mainUI.c
@@ -6,24 +6,27 @@
 
 int main()
 {
-  /*char str[50];
+  char str[50];
   printf("\n> ");
   scanf("%[^\n]",str);
   printf("%s\n",str);
-  */
-
+  printf("The word_start() pointer points to : %c\n",*(word_start(&str[0])));
+  printf("The word starts at: %d \nThe word_terminator() pointer is at: %d\n",&str[0],word_terminator(&str[0]));
+  printf("Now count_words in string: %d\n",(count_words(&str[0])));
+  
+  /* 
   while (1) { // Infinite while loop
 
     fputs("\n> ", stdout);
-    fflush(stdout);/* stdout only flushes automatically on \n */
+    fflush(stdout); 
     int c;
-    while ((c = getchar()) == '\n'); /* ignore newlines */
-    if (c == EOF)     /* terminate on end-of-file */
+    while ((c = getchar()) == '\n');
+    if (c == EOF)      
       goto done;
     
     if(c=='~'){
       puts("Bye!");
-      goto done; /* terminate */
+      goto done; 
     }
     else{
       printf("sc : %d",space_char(c));
@@ -32,4 +35,5 @@ int main()
 }
  done: // To exit from program
   return 0;
+  */
 }
