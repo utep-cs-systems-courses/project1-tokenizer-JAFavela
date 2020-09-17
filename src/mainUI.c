808f44903c6fe38ecaa820420a692442582be93a
JAFavela <jafavela@miners.utep.edu>
2020-09-16 20:10:18 -0600

    Cleaned up all the extra test code

--- src/mainUI.c
+++ src/mainUI.c
@@ -16,39 +16,10 @@ int main()
       printf("Goodbye.\n");
       goto done;
     }
-    //printf("\n%s\n",str);
-    /*
-    printf("The word_start() pointer points to : %c\n",*(word_start(&str[0])));
-    printf("The word starts at: %d \nThe word_terminator() pointer is at: %d\n",&str[0],word_terminator(&str[0]));
-    printf("Count_words() in string: %d\n",(count_words(&str[0])));
-    short ln = word_terminator(&str[0]) - (&str[0]);
-    printf("Copy of first word using copy_str(): %s\n",copy_str(&str[0],ln));
-    */
     char **tokens = (tokenize(&str[0]));
     print_tokens(tokens);
-    // free_tokens(tokens);
+    free_tokens(tokens);
   }
-  
-  /* 
-  while (1) { // Infinite while loop
-
-    fputs("\n> ", stdout);
-    fflush(stdout); 
-    int c;
-    while ((c = getchar()) == '\n');
-    if (c == EOF)      
-      goto done;
-    
-    if(c=='~'){
-      puts("Bye!");
-      goto done; 
-    }
-    else{
-      printf("sc : %d",space_char(c));
-      printf("\nnsc: %d\n",non_space_char(c));
-    }
-  }
-  */
  done: // To exit from program
   return 0;
 }
