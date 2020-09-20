d8ec3f5c3adb5a79c991f805e2c251fbebec9115
JAFavela <jafavela@miners.utep.edu>
2020-09-20 10:21:33 -0600

    Completed main and added ability to make a copy of string

--- src/mainUI.c
+++ src/mainUI.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "tokenizer.h"
 #include "history.h"
 
@@ -6,9 +7,10 @@
 
 int main()
 {
+  char *strCopy;
+  List *history = init_history();
   printf("'~' to exit");
-  while(1){
-    
+  while(1){   
     char str[50];
     printf("\n\n> ");
     scanf(" %[^\n]",str);
@@ -16,10 +18,42 @@ int main()
       printf("Goodbye.\n");
       goto done;
     }
-    char **tokens = (tokenize(&str[0]));
-    print_tokens(tokens);
-    free_tokens(tokens);
+    else if(str[0]=='!'){
+      char *end;
+      long rid= strtol(&str[1], &end, 10);
+      char *hist = get_history(history, (int) rid);
+      if(*hist!='\0'){
+	char **tokens = (tokenize(hist));
+	print_tokens(tokens);
+	free_tokens(tokens);
+      }
+    }
+    else if(str[0]=='?'){
+      print_history(history);
+    }
+    else{
+      char *temp=word_start(&str[0]);
+      int len= 0;
+      while(*temp!='\0'){
+	len++;
+	temp++;
+      }
+      strCopy= malloc(sizeof(char)*(len+1));
+      temp=temp-len;
+      while(*temp!='\0'){
+	*strCopy=*temp;
+	strCopy++;
+	temp++;
+      }
+      *strCopy=*temp;
+      strCopy=strCopy-len;
+      add_history(history,strCopy);
+      char **tokens = (tokenize(strCopy));
+      print_tokens(tokens);
+      free_tokens(tokens);
+    }
   }
  done: // To exit from program
+  free_history(history);
   return 0;
 }
