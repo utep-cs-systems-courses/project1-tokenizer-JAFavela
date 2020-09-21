b569c42695f936c92d7f5bed8602245a3ce01f39
JAFavela <jafavela@miners.utep.edu>
2020-09-20 18:57:56 -0600

    Added some final notes and checked for consistent indentation and spacing

--- src/history.c
+++ src/history.c
@@ -5,7 +5,8 @@
 //This file will include all the methods defined in history.h
 
 /* Initialize the linked list to keep the history. */
-List *init_history(){
+List *init_history()
+{
   List *history = malloc(sizeof(List));
   history->root = malloc(sizeof(Item));
   return history;
@@ -16,63 +17,60 @@ List *init_history(){
    List* list - the linked list
    char* str - the string to store
 */
-void add_history(List *list, char *str){
-  Item *temp = list->root;
-  int nid=0;
-  while(temp->next!=NULL){
+void add_history(List *list, char *str)
+{
+  Item *temp = list->root; //Temp pointer to iterate through LL
+  int nid = 0;
+  while(temp->next != NULL){
     nid++;
-    temp=temp->next;
+    temp = temp->next;
   }
-  temp->id=nid+1;
-  temp->str=str;
-  temp->next= malloc(sizeof(Item));
+  temp->id  = nid+1;
+  temp->str =   str;
+  temp->next = malloc(sizeof(Item)); //allocate memory for next item in LL
 }
 
 /* Retrieve the string stored in the node where Item->id == id.
    List* list - the linked list
    int id - the id of the Item to find */
-char *get_history(List *list, int id){
+char *get_history(List *list, int id)
+{
   Item *temp = list->root;
-  int nid=0;
-  while(temp->id!=id){
-    if(temp->next->next==NULL){
-      printf("Sorry, please enter a valid ID to retrieve string from history");
-      char *err='\0';
-      return err;
-    }
-    temp=temp->next;
+  while(temp->id != id) {
+    temp = temp->next;
   }
   return temp->str;
 }
 
 /*Print the entire contents of the list. */
-void print_history(List *list){
+void print_history(List *list)
+{
   printf("Input History:\nID |  Input\n-------------------------------------------\n");
-  Item *temp= list->root;
+  Item *temp = list->root;
   while(1){
-    if(temp->id<10)
-      printf(" %d |  %s\n",temp->id,temp->str);
+    if(temp->id < 10)
+      printf(" %d |  %s\n", temp->id, temp->str); //added just so history output looked nicer
     else
-      printf("%d |  %s\n",temp->id,temp->str);
-    if(temp->next->next==NULL)
+      printf( "%d |  %s\n", temp->id, temp->str);
+    if(temp->next->next == NULL)
       break;
-    temp=temp->next;
+    temp = temp->next;
   }
-  //printf(" %d |%s\n",temp->id,temp->str);
 }
 
 /*Free the history list and the strings it references. */
-void free_history(List *list){
-  Item *behind= list->root;
+void free_history(List *list)
+{
+  Item *behind = list->root;
   Item *ahead;
-  while(behind->next!=NULL){
-    ahead=behind->next;
+  while(behind->next != NULL){ //frees memory in reverse order
+    ahead = behind->next;
     free(behind->next);
     free(behind);
-    behind=ahead;
+    behind = ahead;
   }
   free(behind->next);
   free(behind);
-  behind=ahead;
+  behind = ahead;
   free(list);
 }
